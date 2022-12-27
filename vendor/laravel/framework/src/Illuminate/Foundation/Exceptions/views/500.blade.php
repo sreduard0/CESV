c2f03ace65d6b3cd7201db49376a608acbe610d4
Eduardo <you@example.com>
2022-12-27 11:34:30 -0300

    up

--- vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/500.blade.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/500.blade.php
@@ -1,5 +1,12 @@
 @extends('errors::minimal')
 
 @section('title', __('Server Error'))
-@section('code', '500')
+@section('code')
+    500
+    <script>
+        setTimeout(() => {
+            window.location.reload(true);
+        }, 6000);
+    </script>
+@endsection
 @section('message', __('Server Error'))
--- vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/503.blade.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/503.blade.php
@@ -1,5 +1,13 @@
 @extends('errors::minimal')
 
 @section('title', __('Service Unavailable'))
-@section('code', '503')
+@section('code')
+    503
+    <script>
+        setTimeout(() => {
+            window.location.reload(true);
+        }, 6000);
+    </script>
+@endsection
 @section('message', __('Service Unavailable'))
+@extends('errors::minimal')
